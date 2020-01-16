3defd36c57cc138db361826b5e6a9a5ee4d1da0f
Renan Queiroz <queirozrrq@gmail.com>
2020-01-15 20:43:41 -0500

    sepolicy: Drop Lineage sepolicy include
    We don't need LOS sepolicy and PA's sepolicy is already included elsewhere.

--- sepolicy.mk
+++ sepolicy.mk
@@ -24,4 +24,3 @@ BOARD_SEPOLICY_DIRS += \
 # Add sepolicy version to support OS upgrade and backward compatibility
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 
--include device/lineage/sepolicy/qcom/sepolicy.mk
